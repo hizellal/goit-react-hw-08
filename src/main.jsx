ac49e04676312d2a435d26f6defcfb926819534f
hizellal <vladimir.liubshenko@gmailcom>
2025-04-25 19:15:54 +0200

    retry

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
+import App from "./components/App/App";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { PersistGate } from "redux-persist/integration/react";
-import App from "./components/App/App";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
